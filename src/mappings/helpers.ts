f09734234ec115bf03146b33a73f702d696472d8
kunal.goyal <kunal.goyal@latoken.com>
2022-07-11 19:32:57 +0530

    changed to ladex testnet factory

--- src/mappings/helpers.ts
+++ src/mappings/helpers.ts
@@ -8,7 +8,7 @@ import { Factory as FactoryContract } from '../types/templates/Pair/Factory'
 import { TokenDefinition } from './tokenDefinition'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0x72392D5F7040DA1fa7A9Dd40bBaBEb6E24f35f2e'
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
